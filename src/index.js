85eecab0b34ca1b7b5680d7e08beb57109f3f733
Super-pyl <92167661+Super-pyl@users.noreply.github.com>
2024-09-02 20:34:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.pangyl.top": dockerHub,
+  "quay.pangyl.top": "https://quay.io",
+  "gcr.pangyl.top": "https://gcr.io",
+  "k8s-gcr.pangyl.top": "https://k8s.gcr.io",
+  "k8s.pangyl.top": "https://registry.k8s.io",
+  "ghcr.pangyl.top": "https://ghcr.io",
+  "cloudsmith.pangyl.top": "https://docker.cloudsmith.io",
+  "ecr.pangyl.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.pangyl.top": dockerHub,
 };
 
 function routeByHosts(host) {
